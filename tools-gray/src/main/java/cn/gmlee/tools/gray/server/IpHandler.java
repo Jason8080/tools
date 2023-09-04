7b8b808fbe2fea071a5e64ee0dcb7294d3671756
liqing <xiaoku13141@163.com>
2023-09-04 16:59:10 +0800

    #gray:v5.2.9::IP支持

--- tools-gray/src/main/java/cn/gmlee/tools/gray/server/IpHandler.java
+++ tools-gray/src/main/java/cn/gmlee/tools/gray/server/IpHandler.java
@@ -1,5 +1,12 @@
 package cn.gmlee.tools.gray.server;
 
+import cn.gmlee.tools.base.util.BoolUtil;
+import cn.gmlee.tools.base.util.UrlUtil;
+import cn.gmlee.tools.gray.mod.App;
+import cn.gmlee.tools.gray.mod.Rule;
+
+import java.util.Map;
+
 /**
  * 地址处理器.
  */
@@ -20,6 +27,15 @@ public class IpHandler extends AbstractGrayHandler {
 
     @Override
     public boolean allow(String app, String ip) {
-        return true;
+        App application = grayServer.properties.getApps().get(app);
+        Map<String, Rule> rules = application.getRules();
+        if(BoolUtil.isEmpty(rules)){
+            return false;
+        }
+        Rule rule = rules.get(name());
+        if(BoolUtil.isNull(rule)){
+            return false;
+        }
+        return UrlUtil.matchOne(rule.getContent(), ip);
     }
 }
