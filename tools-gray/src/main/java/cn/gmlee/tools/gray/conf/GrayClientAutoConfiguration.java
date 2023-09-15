eb8d4ddda09f5984be8b097967f3de64eb633ff7
liqing <xiaoku13141@163.com>
2023-09-15 14:35:25 +0800

    #gray:v5.3.8::代码优化

--- tools-gray/src/main/java/cn/gmlee/tools/gray/conf/GrayClientAutoConfiguration.java
+++ tools-gray/src/main/java/cn/gmlee/tools/gray/conf/GrayClientAutoConfiguration.java
@@ -3,6 +3,7 @@ package cn.gmlee.tools.gray.conf;
 import cn.gmlee.tools.gray.balancer.GrayReactorServiceInstanceLoadBalancer;
 import cn.gmlee.tools.gray.server.GrayServer;
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import lombok.Getter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -25,6 +26,7 @@ import java.util.Map;
 @AutoConfigureBefore(name = {"com.alibaba.cloud.nacos.discovery.NacosDiscoveryClientConfiguration"})
 public class GrayClientAutoConfiguration {
 
+    @Getter
     private final String serviceId;
 
     /**
