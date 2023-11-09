af2d5123ac4d0425a9381d7c1d9f552ee5c7651e
liqing <xiaoku13141@163.com>
2023-11-09 14:44:36 +0800

    #base/webapp:v5.5.0::异常处理:::新增请求超时并统一技术异常

--- tools-webapp/src/main/java/cn/gmlee/tools/webapp/global/GlobalExceptionHandlerAdvice.java
+++ tools-webapp/src/main/java/cn/gmlee/tools/webapp/global/GlobalExceptionHandlerAdvice.java
@@ -1,14 +1,10 @@
 package cn.gmlee.tools.webapp.global;
 
 import cn.gmlee.tools.base.enums.XCode;
-import cn.gmlee.tools.base.ex.AgreedException;
-import cn.gmlee.tools.base.ex.RemoteInvokeException;
 import cn.gmlee.tools.base.ex.SkillException;
-import cn.gmlee.tools.base.ex.VcException;
 import cn.gmlee.tools.base.mod.JsonResult;
 import cn.gmlee.tools.base.util.ExceptionUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Component;
@@ -28,218 +24,94 @@ import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.net.SocketTimeoutException;
 
-/**
- * 全局异常处理
- *
- * @author Jas °
- * @date 2020 /8/28 (周五)
- */
+@Slf4j
 @Component
 @RestControllerAdvice
 public class GlobalExceptionHandlerAdvice {
 
-    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandlerAdvice.class);
-
-    /**
-     * 未知异常
-     *
-     * @param request   the request
-     * @param throwable the throwable
-     * @return json result
-     */
     @ExceptionHandler({Exception.class, Throwable.class})
     public JsonResult throwable(HttpServletRequest request, Throwable throwable) {
-        logger.error("已捕捉: 未知异常\r\n{}", ExceptionUtil.getAllMsg(throwable));
-        return JsonResult.FAIL.newly(throwable.getMessage());
+        log.error("已捕捉: 未知异常\r\n{}", ExceptionUtil.getAllMsg(throwable));
+        return JsonResult.FAIL.newly(throwable);
     }
 
-    /**
-     * 运行时异常.
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return the json result
-     */
     @ExceptionHandler({RuntimeException.class})
     public JsonResult runtimeException(HttpServletRequest request, RuntimeException ex) {
-        logger.warn("已捕捉: 运行时异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly(ex.getMessage());
+        log.warn("已捕捉: 运行时异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        return JsonResult.FAIL.newly(ex);
     }
 
-    /**
-     * 消息读取异常
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return json result
-     */
     @ExceptionHandler({HttpMessageNotReadableException.class})
     public JsonResult invalidFormatException(HttpServletRequest request, HttpMessageNotReadableException ex) {
-        logger.info("已捕捉: 消息读取异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        log.info("已捕捉: 消息读取异常\r\n{}", ExceptionUtil.getAllMsg(ex));
         return JsonResult.FAIL.newly(XCode.REQUEST_FAIL, ExceptionUtil.getOriginMsg(ex));
     }
 
-    /**
-     * 技术异常.
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return the json result
-     */
-    @ExceptionHandler({SkillException.class})
-    public JsonResult skillException(HttpServletRequest request, SkillException ex) {
-        logger.error("已捕捉: 技术异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly(ex.getCode(), ex.getMessage());
-    }
-
-    /**
-     * 需要验证码异常.
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return the json result
-     */
-    @ExceptionHandler({VcException.class})
-    public JsonResult vcException(HttpServletRequest request, VcException ex) {
-        logger.error("已捕捉: 需要输入验证码异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly(ex.getCode(), ex.getMessage());
-    }
-
-
-    /**
-     * 远程调用异常.
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return the json result
-     */
-    @ExceptionHandler({RemoteInvokeException.class})
-    public JsonResult remoteInvokeException(HttpServletRequest request, RemoteInvokeException ex) {
-        logger.error("已捕捉: 远程调用异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly(ex.getCode(), ex.getMessage());
-    }
-
-    /**
-     * 约定响应异常.
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return the json result
-     */
-    @ExceptionHandler({AgreedException.class})
-    public JsonResult agreedException(HttpServletRequest request, AgreedException ex) {
-        logger.info("已捕捉: 约定响应异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly(ex.getCode(), ex.getMessage());
-    }
-
-    /**
-     * 数据效验异常
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return json result
-     */
     @ExceptionHandler({BindException.class})
     public JsonResult validateException(HttpServletRequest request, BindException ex) {
-        logger.info("已捕捉: 数据效验异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        log.info("已捕捉: 数据效验异常\r\n{}", ExceptionUtil.getAllMsg(ex));
         StringBuilder sb = new StringBuilder();
         for (ObjectError oe : ex.getAllErrors()) {
             sb.append(oe.getDefaultMessage());
             sb.append(";");
         }
-        return JsonResult.FAIL.newly(sb.substring(0, sb.length() - 1));
+        return JsonResult.FAIL.newly(XCode.API_PARAM, sb.substring(0, sb.length() - 1));
     }
 
-    /**
-     * 数据效验异常
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return json result
-     */
     @ExceptionHandler({ConstraintViolationException.class})
     public JsonResult validateException(HttpServletRequest request, ConstraintViolationException ex) {
-        logger.info("已捕捉: 数据效验异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        log.info("已捕捉: 数据效验异常\r\n{}", ExceptionUtil.getAllMsg(ex));
         StringBuilder sb = new StringBuilder();
         for (ConstraintViolation cv : ex.getConstraintViolations()) {
             sb.append(cv.getMessage());
             sb.append(";");
         }
-        return JsonResult.FAIL.newly(sb.substring(0, sb.length() - 1));
+        return JsonResult.FAIL.newly(XCode.API_PARAM, sb.substring(0, sb.length() - 1));
     }
 
-    /**
-     * 数据效验异常
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return json result
-     */
     @ExceptionHandler({MethodArgumentNotValidException.class})
     public JsonResult validateException(HttpServletRequest request, MethodArgumentNotValidException ex) {
-        logger.info("已捕捉: 数据效验异常\r\n", ExceptionUtil.getAllMsg(ex));
+        log.info("已捕捉: 数据效验异常\r\n{}", ExceptionUtil.getAllMsg(ex));
         StringBuilder sb = new StringBuilder();
         BindingResult result = ex.getBindingResult();
         for (ObjectError oe : result.getAllErrors()) {
             sb.append(oe.getDefaultMessage());
             sb.append(";");
         }
-        return JsonResult.FAIL.newly(sb.substring(0, sb.length() - 1));
+        return JsonResult.FAIL.newly(XCode.API_PARAM, sb.substring(0, sb.length() - 1));
     }
 
 
-    /**
-     * 数据绑定异常
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return json result
-     */
     @ExceptionHandler({
             ServletRequestBindingException.class, // SpringMVC注解绑定异常
             IllegalStateException.class // 非法参数绑定异常
     })
     public JsonResult slException(HttpServletRequest request, Exception ex) {
-        logger.info("已捕捉: 数据绑定异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly("请求数据绑定异常");
+        log.info("已捕捉: 数据绑定异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        return JsonResult.FAIL.newly(XCode.API_PARAM, ex);
     }
 
-    /**
-     * 参数类型异常
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return json result
-     */
     @ExceptionHandler(TypeMismatchException.class)
     public JsonResult typeMismatchException(HttpServletRequest request, TypeMismatchException ex) {
-        logger.info("已捕捉: 参数类型异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly("不支持的参数类型");
+        log.info("已捕捉: 参数类型异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        return JsonResult.FAIL.newly(XCode.API_PARAM, ex);
     }
 
-    /**
-     * 不支持的请求异常
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return json result
-     */
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class, NoHandlerFoundException.class})
     public JsonResult servletException(HttpServletRequest request, ServletException ex) {
-        logger.info("已捕捉: 不支持的请求异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly("不支持的请求方式");
+        log.info("已捕捉: 不支持的请求异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        return JsonResult.FAIL.newly(XCode.API_METHOD, ex);
     }
 
-    /**
-     * 请求超时异常.
-     *
-     * @param request the request
-     * @param ex      the ex
-     * @return the json result
-     */
     @ExceptionHandler({SocketTimeoutException.class})
     public JsonResult socketTimeoutException(HttpServletRequest request, SocketTimeoutException ex) {
-        logger.error("已捕捉: 请求超时异常\r\n{}", ExceptionUtil.getAllMsg(ex));
-        return JsonResult.FAIL.newly("请求超时");
+        log.error("已捕捉: 请求超时异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        return JsonResult.FAIL.newly(XCode.REQUEST_TIMEOUT, ex);
+    }
+
+    @ExceptionHandler({SkillException.class})
+    public JsonResult skillException(HttpServletRequest request, SkillException ex) {
+        log.error("已捕捉: 技术异常\r\n{}", ExceptionUtil.getAllMsg(ex));
+        return JsonResult.FAIL.newly(ex);
     }
 }
