da5f4f4270f3ccec85ee882add369954816433d1
liqing <xiaoku13141@163.com>
2024-05-10 14:32:51 +0800

    假如航母

--- tools-profile/src/main/java/cn/gmlee/tools/profile/conf/ProfileProperties.java
+++ tools-profile/src/main/java/cn/gmlee/tools/profile/conf/ProfileProperties.java
@@ -7,8 +7,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * Profile参数
  */
 @Data
-@ConfigurationProperties(prefix = "tools.profile")
+@ConfigurationProperties(prefix = ProfileProperties.PREFIX)
 public class ProfileProperties {
+    public static final String PREFIX = "tools.profile";
     private String evn = "env";
     private Boolean enable = Boolean.TRUE;
 }
