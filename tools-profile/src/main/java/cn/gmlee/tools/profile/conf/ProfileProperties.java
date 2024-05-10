9eeb3c0eb271db8ac381d17d69c0a45a84bf6edc
liqing <xiaoku13141@163.com>
2024-05-10 14:56:24 +0800

    配置支持uid

--- tools-profile/src/main/java/cn/gmlee/tools/profile/conf/ProfileProperties.java
+++ tools-profile/src/main/java/cn/gmlee/tools/profile/conf/ProfileProperties.java
@@ -3,6 +3,10 @@ package cn.gmlee.tools.profile.conf;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Profile参数
  */
@@ -12,4 +16,5 @@ public class ProfileProperties {
     public static final String PREFIX = "tools.profile";
     private Boolean open = Boolean.TRUE;
     private String field = "env";
+    private List<String> uid = Collections.emptyList();
 }
