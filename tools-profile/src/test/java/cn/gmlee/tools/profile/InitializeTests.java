aca7c8f8819b2cfc5d1a4c030a45bacc3442b2b0
liqing <xiaoku13141@163.com>
2024-05-17 17:30:31 +0800

    测试代码

--- tools-profile/src/test/java/cn/gmlee/tools/profile/InitializeTests.java
+++ tools-profile/src/test/java/cn/gmlee/tools/profile/InitializeTests.java
@@ -1,12 +1,13 @@
 package cn.gmlee.tools.profile;
 
+import cn.gmlee.tools.base.builder.KvBuilder;
+import cn.gmlee.tools.base.mod.Kv;
+import cn.gmlee.tools.base.util.ExceptionUtil;
 import cn.gmlee.tools.profile.conf.ProfileProperties;
 import cn.gmlee.tools.profile.initializer.OracleProfileDataInitializer;
 import cn.gmlee.tools.profile.initializer.ProfileDataTemplate;
 import oracle.jdbc.pool.OracleDataSource;
 
-import javax.sql.DataSource;
-import java.sql.Driver;
 import java.sql.SQLException;
 
 /**
@@ -17,16 +18,32 @@ public class InitializeTests {
     /**
      * 初始化数据库.
      * <p>
-     *     给所有表增加env字段
+     * 给所有表增加env字段
      * </p>
      *
      * @param args the input arguments
      */
     public static void main(String[] args) throws SQLException {
+        // 注意环境地址
+        String url = "jdbc:oracle:thin:@//scan-ip.exgrain.org:1521/ygwdb";
+        // 开始批量处理
+        handleBatch(url, KvBuilder.array("cpp", "cpp1234"));
+    }
+
+    private static void handleBatch(String url, Kv<String, String>... kvs) throws SQLException {
+        for (Kv<String, String> kv : kvs) {
+            System.out.println(String.format("%s::::::::::%s", kv.getKey(), kv.getKey()));
+            System.out.println("---------- Start. ---------- ");
+            ExceptionUtil.sandbox(() -> handleOnce(url, kv));
+            System.out.println("---------- Done. ---------- ");
+        }
+    }
+
+    private static void handleOnce(String url, Kv<String, String> kv) throws SQLException {
         OracleDataSource dataSource = new OracleDataSource();
-        dataSource.setURL("jdbc:oracle:thin:@//scan-ip.exgrain.org:1521/ygwdb");
-        dataSource.setUser("cpp");
-        dataSource.setPassword("cpp1234");
+        dataSource.setURL(url);
+        dataSource.setUser(kv.getKey());
+        dataSource.setPassword(kv.getVal());
         ProfileProperties properties = new ProfileProperties();
         ProfileDataTemplate profileDataTemplate = new ProfileDataTemplate(dataSource, properties);
         profileDataTemplate.init(new OracleProfileDataInitializer());
