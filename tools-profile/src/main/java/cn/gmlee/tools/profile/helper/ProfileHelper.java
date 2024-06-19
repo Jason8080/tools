2c831085891f6d5381d7f501c4897421fb2d69fb
liqing <xiaoku13141@163.com>
2024-06-19 17:06:38 +0800

    代码优化: 支持读写分离&环境集合

--- tools-profile/src/main/java/cn/gmlee/tools/profile/helper/ProfileHelper.java
+++ tools-profile/src/main/java/cn/gmlee/tools/profile/helper/ProfileHelper.java
@@ -1,8 +1,9 @@
 package cn.gmlee.tools.profile.helper;
 
-import cn.gmlee.tools.base.util.NullUtil;
-import cn.gmlee.tools.base.util.WebUtil;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.*;
 
 /**
  * 环境帮助类.
@@ -11,28 +12,82 @@ public class ProfileHelper {
 
     private static volatile Boolean open = Boolean.TRUE;
 
-    private static ThreadLocal<Boolean> read = new InheritableThreadLocal<>();
-    private static ThreadLocal<Boolean> write = new InheritableThreadLocal<>();
+    private static ThreadLocal<Map<ReadWrite, Set<Env>>> env = new InheritableThreadLocal<>();
 
+    /**
+     * The enum Read write.
+     */
     @Getter
     public enum ReadWrite {
+        /**
+         * Read read write.
+         */
         READ,
+        /**
+         * Write read write.
+         */
         WRITE,
         ;
     }
 
+    /**
+     * The enum Env.
+     */
+    @RequiredArgsConstructor
+    public enum Env {
+        /**
+         * Stg env.
+         */
+        STG(0),
+        /**
+         * Prd env.
+         */
+        PRD(1),
+        ;
+        /**
+         * The Value.
+         */
+        public final Integer value;
+    }
 
     /**
-     * 启禁.
+     * 添加环境.
      *
-     * @return the boolean
+     * @param rw   the rw
+     * @param envs the envs
      */
-    public static void enable(ReadWrite rw) {
-        if (ReadWrite.READ.equals(rw)) {
-            ProfileHelper.read.set(true);
-        } else if (ReadWrite.WRITE.equals(rw)) {
-            ProfileHelper.write.set(true);
+    public static void add(ReadWrite rw, Env... envs) {
+        Map<ReadWrite, Set<Env>> map = env.get();
+        if (map == null) {
+            map = new HashMap<>();
+            env.set(map);
         }
+        Set<Env> set = map.get(rw);
+        if (set == null) {
+            set = new HashSet<>();
+            map.put(rw, set);
+        }
+        if (envs != null && envs.length > 0) {
+            set.addAll(Arrays.asList(envs));
+        }
+    }
+
+    /**
+     * 获取环境.
+     *
+     * @param rw the rw
+     * @return set set
+     */
+    public static Set<Env> get(ReadWrite rw) {
+        Map<ReadWrite, Set<Env>> map = env.get();
+        if (map == null) {
+            return Collections.emptySet();
+        }
+        Set<Env> set = map.get(rw);
+        if (set == null) {
+            return Collections.emptySet();
+        }
+        return set;
     }
 
     /**
@@ -41,23 +96,22 @@ public class ProfileHelper {
      * @return the boolean
      */
     public static void remove() {
-        ProfileHelper.read.remove();
-        ProfileHelper.write.remove();
+        ProfileHelper.env.remove();
     }
 
     /**
-     * 是否已开启.
+     * 是否启用STG环境.
      *
+     * @param rw the rw
      * @return the boolean
      */
     public static boolean enabled(ReadWrite rw) {
-        // 系统兜底视角: 读正式+测试、写正式
-        if (!WebUtil.isWeb()) {
-            Boolean read = NullUtil.get(ProfileHelper.read.get(), true);
-            Boolean write = NullUtil.get(ProfileHelper.write.get(), false);
-            return ReadWrite.READ.equals(rw) ? Boolean.TRUE.equals(read) : Boolean.TRUE.equals(write);
+        Set<Env> envs = get(rw);
+        if(envs.isEmpty()){
+            // 默认启用生产环境
+            envs.add(Env.PRD);
         }
-        return ReadWrite.READ.equals(rw) ? Boolean.TRUE.equals(read.get()) : Boolean.TRUE.equals(write.get());
+        return envs.contains(Env.STG);
     }
 
     // -----------------------------------------------------------------------------------------------------------------
@@ -65,6 +119,7 @@ public class ProfileHelper {
     /**
      * 开关.
      *
+     * @param open the open
      * @return the boolean
      */
     public static void open(boolean open) {
