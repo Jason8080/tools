783531815e9f6856cfc64fd1da75ee68d88075a8
liqing <xiaoku13141@163.com>
2024-06-20 16:21:06 +0800

    提供深拷贝工具

--- tools-base/src/main/java/cn/gmlee/tools/base/util/CopyUtil.java
+++ tools-base/src/main/java/cn/gmlee/tools/base/util/CopyUtil.java
@@ -1,6 +1,7 @@
 package cn.gmlee.tools.base.util;
 
 import java.io.*;
+import java.util.Arrays;
 
 /**
  * 对象深拷贝工具.
@@ -28,5 +29,25 @@ public class CopyUtil {
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException("Deep copy error", e);
         }
+    }
+
+    /**
+     * Get t [ ].
+     *
+     * @param <T> the type parameter
+     * @param os  the os
+     * @return the t [ ]
+     */
+    public static <T> T[] get(T... os) {
+        // 空组不处理
+        if (BoolUtil.isEmpty(os)) {
+            return os;
+        }
+        // 创建新数组
+        T[] newOs = Arrays.copyOf(os, os.length);
+        for (int i = 0; i < newOs.length; i++) {
+            newOs[i] = get(os[i]);
+        }
+        return newOs;
     }
 }
--- tools-base/src/test/java/cn/gmlee/tools/base/CopyTests.java
+++ tools-base/src/test/java/cn/gmlee/tools/base/CopyTests.java
@@ -2,13 +2,13 @@ package cn.gmlee.tools.base;
 
 import cn.gmlee.tools.base.mod.User;
 import cn.gmlee.tools.base.util.CopyUtil;
+import org.junit.Test;
 
 import java.util.Arrays;
 
 public class CopyTests {
-
-
-    public static void main(String[] args) {
+    @Test
+    public void testOne(){
         User o1 = new User();
         o1.setName("O1");
         User o2 = new User();
@@ -19,5 +19,21 @@ public class CopyTests {
         System.out.println(o1);
         User o = CopyUtil.get(o1);
         System.out.println(o);
+    }
+    @Test
+    public void testArray(){
+        User o1 = new User();
+        o1.setName("O1");
+        User o2 = new User();
+        o2.setName("O2");
+        User o3 = new User();
+        o3.setName("O3");
+        o1.setPartners(Arrays.asList(o2, o3));
+        User[] array = {o1, o2, o3};
+        System.out.println(array);
+        System.out.println(Arrays.toString(array));
+        User[] newArray = CopyUtil.get(array);
+        System.out.println(Arrays.toString(newArray));
+        System.out.println(newArray);
     }
 }
