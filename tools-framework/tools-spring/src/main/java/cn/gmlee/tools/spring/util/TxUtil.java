aaaec3a5f2a3e3c6a647756f215f836e169aac9d
liqing <xiaoku13141@163.com>
2024-08-12 11:12:06 +0800

    添加事务工具

--- tools-framework/tools-spring/src/main/java/cn/gmlee/tools/spring/util/TxUtil.java
+++ tools-framework/tools-spring/src/main/java/cn/gmlee/tools/spring/util/TxUtil.java
@@ -1,5 +1,6 @@
 package cn.gmlee.tools.spring.util;
 
+import cn.gmlee.tools.base.util.NullUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -11,10 +12,11 @@ public class TxUtil {
     /**
      * 打印当前事务信息.
      */
-    public static void print() {
+    public static void print(String... tips) {
+        String tip = String.join(" • ", NullUtil.get(tips, new String[]{"事务信息"}));
         boolean actualTransactionActive = TransactionSynchronizationManager.isActualTransactionActive();
         String currentTransactionName = TransactionSynchronizationManager.getCurrentTransactionName();
         Integer currentTransactionIsolationLevel = TransactionSynchronizationManager.getCurrentTransactionIsolationLevel();
-        log.info("Transaction active: {}, name: {}, level: {}", actualTransactionActive, currentTransactionName, currentTransactionIsolationLevel);
+        log.info("{} -> active: {}, name: {}, level: {}", tip, actualTransactionActive, currentTransactionName, currentTransactionIsolationLevel);
     }
 }
