4fdc9a216580d36acf24d1b4f3c866e22085a27c
liqing <xiaoku13141@163.com>
2024-09-19 13:48:57 +0800

    优化

--- tools-framework/tools-spring/src/main/java/cn/gmlee/tools/spring/mvc/DynamicController.java
+++ tools-framework/tools-spring/src/main/java/cn/gmlee/tools/spring/mvc/DynamicController.java
@@ -1,7 +1,19 @@
 package cn.gmlee.tools.spring.mvc;
 
-public interface DynamicController {
+/**
+ * 动态控制器定义.
+ *
+ * @param <P> the type parameter
+ * @param <R> the type parameter
+ */
+public interface DynamicController<P, R> {
 
-    <P, R> R handle(P p);
+    /**
+     * 动态接口定义.
+     *
+     * @param p 动态参数定义
+     * @return 动态相应定义
+     */
+    R handle(P p);
 
 }
