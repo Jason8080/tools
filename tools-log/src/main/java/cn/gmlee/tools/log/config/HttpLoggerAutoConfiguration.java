ccc0f4f4634542a69839f4a1573239c40b19dc45
liqing <xiaoku13141@163.com>
2024-09-24 14:53:08 +0800

    代码优化

--- tools-log/src/main/java/cn/gmlee/tools/log/config/HttpLoggerAutoConfiguration.java
+++ tools-log/src/main/java/cn/gmlee/tools/log/config/HttpLoggerAutoConfiguration.java
@@ -2,22 +2,29 @@ package cn.gmlee.tools.log.config;
 
 import cn.gmlee.tools.log.http.HttpClientInterceptor;
 import cn.gmlee.tools.log.http.OkHttpInterceptor;
-import okhttp3.Interceptor;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 
+@ImportAutoConfiguration({
+        HttpLoggerAutoConfiguration.OkHttpAutoConfiguration.class,
+        HttpLoggerAutoConfiguration.HttpClientAutoConfiguration.class,
+})
 public class HttpLoggerAutoConfiguration {
 
-    @Bean
-    @ConditionalOnClass(Interceptor.class)
-    public OkHttpInterceptor okHttpInterceptor(){
-        return new OkHttpInterceptor();
+    @ConditionalOnClass(name = "okhttp3.Interceptor")
+    public static class OkHttpAutoConfiguration {
+        @Bean
+        public OkHttpInterceptor okHttpInterceptor(){
+            return new OkHttpInterceptor();
+        }
     }
 
-    @Bean
-    @ConditionalOnClass(ClientHttpRequestInterceptor.class)
-    public HttpClientInterceptor clientHttpRequestInterceptor(){
-        return new HttpClientInterceptor();
+    @ConditionalOnClass(name = "org.springframework.http.client.ClientHttpRequestInterceptor")
+    public static class HttpClientAutoConfiguration {
+        @Bean
+        public HttpClientInterceptor clientHttpRequestInterceptor(){
+            return new HttpClientInterceptor();
+        }
     }
 }
