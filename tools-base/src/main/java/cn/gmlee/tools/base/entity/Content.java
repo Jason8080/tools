c7c92302a66343956ca962f28599526cdd376c08
liqing <xiaoku13141@163.com>
2025-03-03 13:36:04 +0800

    添加实体类

---
+++ tools-base/src/main/java/cn/gmlee/tools/base/entity/Content.java
@@ -0,0 +1,30 @@
+package cn.gmlee.tools.base.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * 通用内容实体.
+ *
+ * @author Jas°
+ * @date 2021/11/10 (周三)
+ */
+@Getter
+@Setter
+@ToString
+public class Content extends Id implements Serializable {
+    /**
+     * 内容
+     */
+    public String content;
+
+    public Content() {
+    }
+
+    public Content(String content) {
+        this.content = content;
+    }
+}
