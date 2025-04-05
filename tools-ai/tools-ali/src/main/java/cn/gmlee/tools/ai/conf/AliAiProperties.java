f4767dc45e2b2a518b80e7240995647c13c07157
Jason° <xiaoku13141@163.com>
2025-04-05 22:46:54 +0800

    代码优化

--- tools-ai/tools-ali/src/main/java/cn/gmlee/tools/ai/conf/AliAiProperties.java
+++ tools-ai/tools-ali/src/main/java/cn/gmlee/tools/ai/conf/AliAiProperties.java
@@ -6,8 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties(prefix = "tools.ai.ali")
 public class AliAiProperties {
-    private String spaceId = "llm-tr5o0shrj1159vlm";
-    private String agentKey = "00d39a41238d44faa30f5213360ebeca_p_efm";
-    private String apiKey = "sk-7793b148079149f887608357e7f5b2e0";
-    private String aliModel = "qwen-omni-turbo";
+    private String spaceId;
+    private String agentKey;
+    private String appId;
+    private String apiKey;
+    private String aliModel;
 }
