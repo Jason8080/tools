97fff5eee2c3ea025f86c026b86c77331cd2485c
liqing <xiaoku13141@163.com>
2025-04-22 20:52:53 +0800

    添加智能体API

--- tools-ai/tools-ali/src/test/java/cn/gmlee/tools/ai/ApplicationWlTests.java
+++ tools-ai/tools-ali/src/test/java/cn/gmlee/tools/ai/ApplicationWlTests.java
@@ -22,14 +22,18 @@ public class ApplicationWlTests {
     private static final String user = "提取净重";
 
     // 1911610483048722433
-    private static final String image =
+    private static final String image1 =
             "https://prod-public-ldw.oss-cn-shenzhen.aliyuncs.com/%E6%A8%AA%E5%90%91%E9%94%99%E8%A1%8C%E7%A3%85%E5%8D%951.png?Expires=11745231268&OSSAccessKeyId=LTAI5t6MaMnjGd7qKm9XjjbN&Signature=vv4Zt%2BzO9L2oWJ%2BAbs%2BkIECQBcQ%3D";
+
+    // 1914661974219173889
+    private static final String image2 =
+            "https://prod-public-ldw.oss-cn-shenzhen.aliyuncs.com/%E6%A8%AA%E5%90%91%E9%94%99%E8%A1%8C%E7%A3%85%E5%8D%952.png?Expires=11745325968&OSSAccessKeyId=LTAI5t6MaMnjGd7qKm9XjjbN&Signature=0ZEFXyXFR1P5SRVlUbzEqkhh%2B18%3D";
 
 
     @Test
     public void testAsk(){
         TimerUtil.print();
-        Flowable<String> ask = applicationServer.ask(user, KvBuilder.array("image", image));
+        Flowable<String> ask = applicationServer.ask(user, KvBuilder.array("image", image1));
         StringBuilder sb = new StringBuilder();
         ask.blockingForEach(x -> sb.append(x));
         System.out.println(sb);
