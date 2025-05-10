06044463c6035edfb508fec210a1e0e1a5671f7e
Jason° <xiaoku13141@163.com>
2025-05-10 15:30:18 +0800

    优化登录服务

--- tools-webapp/src/main/java/cn/gmlee/tools/webapp/controller/AuthController.java
+++ tools-webapp/src/main/java/cn/gmlee/tools/webapp/controller/AuthController.java
@@ -3,7 +3,7 @@ package cn.gmlee.tools.webapp.controller;
 import cn.gmlee.tools.base.enums.XCode;
 import cn.gmlee.tools.base.ex.SkillException;
 import cn.gmlee.tools.base.mod.Login;
-import cn.gmlee.tools.webapp.service.LoginService;
+import cn.gmlee.tools.webapp.service.LoginServer;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,16 +19,17 @@ import java.util.Set;
  * 通用鉴权控制器
  *
  * @param <U> 用户泛型
+ * @param <P> the type parameter
  * @param <S> 系统设置泛型
  * @param <D> the type parameter
  * @param <C> 用户配置泛型
- * @author Jas°
+ * @author Jas °
  * @date 2020 /8/28 (周五)
  */
 @Validated
 public class AuthController<U, P, S, D, C> extends ParameterController {
     @Resource
-    protected LoginService loginService;
+    protected LoginServer loginServer;
 
     protected U user;
     protected Map<String, S> settings;
@@ -52,7 +53,7 @@ public class AuthController<U, P, S, D, C> extends ParameterController {
             throw new SkillException(XCode.LOGIN_TIMEOUT.code, XCode.LOGIN_TIMEOUT.msg);
         }
         // 登陆效验
-        Login<U, S, D, C> old = loginService.getLogin(token);
+        Login<U, S, D, C> old = loginServer.getLogin(token);
         if (Objects.isNull(old)) {
             throw new SkillException(XCode.LOGIN_TIMEOUT.code, XCode.LOGIN_TIMEOUT.msg);
         }
