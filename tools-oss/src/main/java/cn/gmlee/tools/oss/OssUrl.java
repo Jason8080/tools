4bc990ee30813e0bfe3d8f834ffec962020bfe41
liqing <xiaoku13141@163.com>
2025-05-12 09:07:40 +0800

    ...

--- tools-oss/src/main/java/cn/gmlee/tools/oss/OssUrl.java
+++ tools-oss/src/main/java/cn/gmlee/tools/oss/OssUrl.java
@@ -1,6 +1,5 @@
 package cn.gmlee.tools.oss;
 
-import cn.gmlee.tools.base.builder.KvBuilder;
 import cn.gmlee.tools.base.builder.MapBuilder;
 import cn.gmlee.tools.base.util.LocalDateTimeUtil;
 import com.aliyun.oss.HttpMethod;
@@ -10,12 +9,23 @@ import com.aliyun.oss.model.GeneratePresignedUrlRequest;
 import java.net.URL;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
-import java.util.Map;
 
 /**
  * OSS 链接生成工具.
  */
 public class OssUrl {
+
+    /**
+     * 获取文件链接.
+     *
+     * @param oss     the oss
+     * @param request the request
+     * @return the url
+     */
+    public static URL get(OSS oss, GeneratePresignedUrlRequest request) {
+        return oss.generatePresignedUrl(request);
+    }
+
     /**
      * 文件上传链接.
      *
@@ -60,5 +70,24 @@ public class OssUrl {
     public static URL download(OSS oss, String bucketName, String objectName, int duration) {
         Date date = LocalDateTimeUtil.offsetCurrent(duration, ChronoUnit.SECONDS);
         return oss.generatePresignedUrl(bucketName, objectName, date, HttpMethod.GET);
+    }
+
+
+    /**
+     * 文件下载链接.
+     *
+     * @param oss        客户端
+     * @param bucketName 对象桶
+     * @param objectName 路径名 (填写Object完整路径,不能包含Bucket名称)
+     * @param duration   有效期
+     * @param headers    请求头
+     * @return string 下载链接
+     */
+    public static URL download(OSS oss, String bucketName, String objectName, int duration, String... headers) {
+        Date date = LocalDateTimeUtil.offsetCurrent(duration, ChronoUnit.SECONDS);
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucketName, objectName, HttpMethod.GET);
+        request.setExpiration(date);
+        request.setHeaders(MapBuilder.of(headers));
+        return oss.generatePresignedUrl(request);
     }
 }
