d0d4e21b430d30dcac019cf4e3ce6a2f52907959
Jason° <xiaoku13141@163.com>
2025-05-28 23:56:10 +0800

    ...

--- tools-framework/tools-microphone/src/main/java/cn/gmlee/tools/microphone/conf/RSocketAutoConfiguration.java
+++ tools-framework/tools-microphone/src/main/java/cn/gmlee/tools/microphone/conf/RSocketAutoConfiguration.java
@@ -1,13 +1,7 @@
 package cn.gmlee.tools.microphone.conf;
 
-import org.springframework.boot.rsocket.messaging.RSocketStrategiesCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.codec.ByteBufferDecoder;
-import org.springframework.core.codec.ByteBufferEncoder;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
 
 /**
  * RSocket自动装配.
@@ -15,13 +9,4 @@ import org.springframework.http.codec.json.Jackson2JsonEncoder;
 @Configuration(proxyBeanMethods = false)
 @PropertySource(value = {"classpath:microphone.properties", "classpath:microphone.properties"}, ignoreResourceNotFound = true)
 public class RSocketAutoConfiguration {
-
-    @Bean
-    public RSocketStrategiesCustomizer strategiesCustomizer() {
-        return strategies -> strategies
-                .decoder(new Jackson2JsonDecoder()) // 处理String部分
-                .decoder(new ByteBufferDecoder())   // 处理byte[]部分
-                .encoder(new Jackson2JsonEncoder()) // 处理String部分
-                .encoder(new ByteBufferEncoder());  // 处理byte[]部分
-    }
 }
