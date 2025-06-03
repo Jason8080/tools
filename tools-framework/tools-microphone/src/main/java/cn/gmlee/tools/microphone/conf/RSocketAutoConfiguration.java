f2160b4b2488e91532417180c89165c4fdae5ed4
liqing <xiaoku13141@163.com>
2025-06-03 12:44:31 +0800

    元数据绑定配置

--- tools-framework/tools-microphone/src/main/java/cn/gmlee/tools/microphone/conf/RSocketAutoConfiguration.java
+++ tools-framework/tools-microphone/src/main/java/cn/gmlee/tools/microphone/conf/RSocketAutoConfiguration.java
@@ -1,7 +1,13 @@
 package cn.gmlee.tools.microphone.conf;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.util.MimeType;
+
+import java.util.Map;
 
 /**
  * RSocket自动装配.
@@ -9,4 +15,16 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration(proxyBeanMethods = false)
 @PropertySource(value = {"classpath:microphone.properties", "classpath:microphone.properties"}, ignoreResourceNotFound = true)
 public class RSocketAutoConfiguration {
+    @Bean
+    public RSocketStrategies rsocketStrategies() {
+        return RSocketStrategies.builder()
+                .metadataExtractorRegistry(registry -> {
+                    registry.metadataToExtract(
+                            MimeType.valueOf("application/json"),
+                            new ParameterizedTypeReference<Map<String, Object>>() {
+                            }, "headers"
+                    );
+                })
+                .build();
+    }
 }
