e38653a922fec94f85ed7069d83480a921383f9b
liqing <xiaoku13141@163.com>
2025-06-18 17:53:28 +0800

    临时提交

--- tools-ai/tools-bit/src/main/java/cn/gmlee/tools/ai/server/stream/MultiModalConversationServer.java
+++ tools-ai/tools-bit/src/main/java/cn/gmlee/tools/ai/server/stream/MultiModalConversationServer.java
@@ -1,30 +1,17 @@
 package cn.gmlee.tools.ai.server.stream;
 
-import cn.gmlee.tools.ai.assist.MultiAssist;
-import cn.gmlee.tools.ai.conf.AliAiProperties;
 import cn.gmlee.tools.ai.conf.BitAiProperties;
-import cn.gmlee.tools.ai.mod.Ask;
-import cn.gmlee.tools.base.util.BoolUtil;
-import cn.gmlee.tools.base.util.ExceptionUtil;
-import cn.gmlee.tools.base.util.NullUtil;
-import com.alibaba.dashscope.common.MultiModalMessage;
-import com.alibaba.dashscope.common.Role;
-import com.alibaba.dashscope.embeddings.MultiModalEmbeddingItemAudio;
-import com.alibaba.dashscope.embeddings.MultiModalEmbeddingItemImage;
-import com.alibaba.dashscope.embeddings.MultiModalEmbeddingItemText;
-import com.alibaba.dashscope.embeddings.MultiModalEmbeddingItemVideo;
 import com.volcengine.ark.runtime.model.completion.chat.ChatCompletionChunk;
 import com.volcengine.ark.runtime.model.completion.chat.ChatCompletionRequest;
 import com.volcengine.ark.runtime.model.completion.chat.ChatMessage;
 import com.volcengine.ark.runtime.model.completion.chat.ChatMessageRole;
 import com.volcengine.ark.runtime.service.ArkService;
-import com.volcengine.ark.runtime.utils.SSE;
 import io.reactivex.Flowable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 方舟平台服务.
