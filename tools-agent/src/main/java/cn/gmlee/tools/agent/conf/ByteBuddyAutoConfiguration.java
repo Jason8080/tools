cde7c30621f137e491044420f291aaa94defbebb
liqing <xiaoku13141@163.com>
2025-08-12 11:38:47 +0800

    代码优化

---
+++ tools-agent/src/main/java/cn/gmlee/tools/agent/conf/ByteBuddyAutoConfiguration.java
@@ -0,0 +1,78 @@
+package cn.gmlee.tools.agent.conf;
+
+import cn.gmlee.tools.agent.bytebuddy.ByteBuddyAdvice;
+import cn.gmlee.tools.base.util.ExceptionUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.NamedElement;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.lang.instrument.Instrumentation;
+import java.util.List;
+
+/**
+ * 方法超时监控自动装配.
+ */
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(MonitorMethodProperties.class)
+public class ByteBuddyAutoConfiguration {
+
+    private final ApplicationContext applicationContext;
+    private final MonitorMethodProperties monitorMethodProperties;
+
+    /**
+     * Init.
+     */
+    @PostConstruct
+    public void init() {
+        ExceptionUtil.sandbox(this::install);
+    }
+
+    private void install() {
+        log.info("[Tools ByteBuddy] Timing Agent is initializing...");
+
+        Instrumentation instrumentation = ByteBuddyAgent.install();
+        if (instrumentation == null) {
+            log.error("Failed to install ByteBuddy agent");
+            return;
+        }
+
+        new AgentBuilder.Default().ignore(ignore()).type(type())
+                .transform((builder, typeDescription, classLoader, module) ->
+                        builder.visit(Advice.to(ByteBuddyAdvice.class).on(ElementMatchers.isMethod()
+                                .and(ElementMatchers.not(ElementMatchers.isConstructor()))
+                                .and(ElementMatchers.not(ElementMatchers.nameStartsWith("lambda$"))))))
+                .installOn(instrumentation);
+
+        log.info("[Tools ByteBuddy] Timing Agent installed.");
+    }
+
+    private ElementMatcher<? super TypeDescription> type() {
+        ElementMatcher.Junction<NamedElement> emj = ElementMatchers.nameStartsWith("net.bytebuddy.");
+        List<String> packages = monitorMethodProperties.getPackages();
+        for (String pack : packages) {
+            emj = emj.or(ElementMatchers.nameStartsWith(pack));
+        }
+        return emj;
+    }
+
+    private ElementMatcher<? super TypeDescription> ignore() {
+        ElementMatcher.Junction<NamedElement> emj = ElementMatchers.nameStartsWith("net.bytebuddy.");
+        List<String> packages = monitorMethodProperties.getIgnorePackages();
+        for (String pack : packages) {
+            emj = emj.or(ElementMatchers.nameStartsWith(pack));
+        }
+        return emj;
+    }
+}
--- tools-agent/src/main/java/cn/gmlee/tools/agent/conf/MonitorTimingAutoConfiguration.java
+++ tools-agent/src/main/java/cn/gmlee/tools/agent/conf/MonitorTimingAutoConfiguration.java
@@ -1,31 +1,19 @@
 package cn.gmlee.tools.agent.conf;
 
-import cn.gmlee.tools.agent.bytebuddy.ByteBuddyAdvice;
 import cn.gmlee.tools.agent.mod.Watcher;
 import cn.gmlee.tools.agent.trigger.ByteBuddyTrigger;
 import cn.gmlee.tools.agent.trigger.TimeoutTrigger;
 import cn.gmlee.tools.agent.watcher.TimeoutWatcher;
-import cn.gmlee.tools.base.util.ExceptionUtil;
 import cn.gmlee.tools.base.util.NullUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.asm.Advice;
-import net.bytebuddy.description.NamedElement;
-import net.bytebuddy.description.type.TypeDescription;
-import net.bytebuddy.matcher.ElementMatcher;
-import net.bytebuddy.matcher.ElementMatchers;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
-import java.lang.instrument.Instrumentation;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * 方法超时监控自动装配.
@@ -34,7 +22,7 @@ import java.util.List;
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties(MonitorMethodProperties.class)
-public class MonitorMethodTimingAutoConfiguration {
+public class MonitorTimingAutoConfiguration {
 
     private final ApplicationContext applicationContext;
     private final MonitorMethodProperties monitorMethodProperties;
@@ -86,50 +74,5 @@ public class MonitorMethodTimingAutoConfiguration {
                 );
             }
         };
-    }
-
-    /**
-     * Init.
-     */
-    @PostConstruct
-    public void init() {
-        ExceptionUtil.sandbox(this::install);
-    }
-
-    private void install() {
-        log.info("[Tools ByteBuddy] Timing Agent is initializing...");
-
-        Instrumentation instrumentation = ByteBuddyAgent.install();
-        if (instrumentation == null) {
-            log.error("Failed to install ByteBuddy agent");
-            return;
-        }
-
-        new AgentBuilder.Default().ignore(ignore()).type(type())
-                .transform((builder, typeDescription, classLoader, module) ->
-                        builder.visit(Advice.to(ByteBuddyAdvice.class).on(ElementMatchers.isMethod()
-                                .and(ElementMatchers.not(ElementMatchers.isConstructor()))
-                                .and(ElementMatchers.not(ElementMatchers.nameStartsWith("lambda$"))))))
-                .installOn(instrumentation);
-
-        log.info("[Tools ByteBuddy] Timing Agent installed.");
-    }
-
-    private ElementMatcher<? super TypeDescription> type() {
-        ElementMatcher.Junction<NamedElement> emj = ElementMatchers.nameStartsWith("net.bytebuddy.");
-        List<String> packages = monitorMethodProperties.getPackages();
-        for (String pack : packages) {
-            emj = emj.or(ElementMatchers.nameStartsWith(pack));
-        }
-        return emj;
-    }
-
-    private ElementMatcher<? super TypeDescription> ignore() {
-        ElementMatcher.Junction<NamedElement> emj = ElementMatchers.nameStartsWith("net.bytebuddy.");
-        List<String> packages = monitorMethodProperties.getIgnorePackages();
-        for (String pack : packages) {
-            emj = emj.or(ElementMatchers.nameStartsWith(pack));
-        }
-        return emj;
     }
 }
