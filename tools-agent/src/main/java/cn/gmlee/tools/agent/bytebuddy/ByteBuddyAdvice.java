ddc374b4dd7da454c13c4d4d93f91107514cd724
liqing <xiaoku13141@163.com>
2025-08-28 11:12:05 +0800

    代码优化

--- tools-agent/src/main/java/cn/gmlee/tools/agent/bytebuddy/ByteBuddyAdvice.java
+++ tools-agent/src/main/java/cn/gmlee/tools/agent/bytebuddy/ByteBuddyAdvice.java
@@ -1,6 +1,5 @@
 package cn.gmlee.tools.agent.bytebuddy;
 
-import cn.gmlee.tools.agent.mod.Watcher;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.implementation.bytecode.assign.Assigner;
 
@@ -9,12 +8,12 @@ import java.lang.reflect.Method;
 public class ByteBuddyAdvice {
 
     @Advice.OnMethodEnter
-    public static Watcher onEnter(@Advice.This Object obj, @Advice.Origin Method method, @Advice.AllArguments Object[] args) {
-        return ByteBuddyRegistry.enter(Watcher.of(obj, method, args));
+    public static Object onEnter(@Advice.This Object obj, @Advice.Origin Method method, @Advice.AllArguments Object[] args) {
+        return ByteBuddyRegistry.enter(obj, method, args);
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class)
-    public static void onExit(@Advice.Enter Watcher watcher, @Advice.Return(typing = Assigner.Typing.DYNAMIC) Object ret, @Advice.Thrown Throwable throwable) {
-        ByteBuddyRegistry.exit(Watcher.ret(watcher, ret, throwable));
+    public static void onExit(@Advice.Enter Object watcher, @Advice.Return(typing = Assigner.Typing.DYNAMIC) Object ret, @Advice.Thrown Throwable throwable) {
+        ByteBuddyRegistry.exit(watcher, ret, throwable);
     }
 }
--- tools-agent/src/main/java/cn/gmlee/tools/agent/bytebuddy/ByteBuddyRegistry.java
+++ tools-agent/src/main/java/cn/gmlee/tools/agent/bytebuddy/ByteBuddyRegistry.java
@@ -6,6 +6,7 @@ import cn.gmlee.tools.agent.trigger.ByteBuddyTrigger;
 import cn.gmlee.tools.base.util.QuickUtil;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,13 +20,23 @@ public class ByteBuddyRegistry {
         return WATCHERS;
     }
 
-    public static Watcher enter(Watcher watcher) {
+    public static Object enter(Object obj, Method method, Object[] args) {
+        return enter(Watcher.of(obj, method, args));
+    }
+
+    private static Watcher enter(Watcher watcher) {
         WATCHERS.add(watcher);
         TriggerAssist.register(watcher, ByteBuddyTrigger::enter);
         return watcher;
     }
 
-    public static void exit(Watcher watcher) {
+    public static void exit(Object watcher, Object ret, Throwable throwable) {
+        if(watcher instanceof Watcher){
+            exit(Watcher.ret((Watcher) watcher, ret, throwable));
+        }
+    }
+
+    private static void exit(Watcher watcher) {
         boolean remove = WATCHERS.remove(watcher);
         QuickUtil.isFalse(remove, () -> log.error("监控方法删除失败: {}", watcher));
         TriggerAssist.register(watcher, ByteBuddyTrigger::exit);
