4161ec86dffcf256d96a5af34fca6382ebfd4eaf
liqing <xiaoku13141@163.com>
2025-09-23 21:05:26 +0800

    代码优化2

--- tools-agent/src/main/java/cn/gmlee/tools/agent/bytebuddy/ByteBuddyAdvice.java
+++ tools-agent/src/main/java/cn/gmlee/tools/agent/bytebuddy/ByteBuddyAdvice.java
@@ -1,5 +1,6 @@
 package cn.gmlee.tools.agent.bytebuddy;
 
+import cn.gmlee.tools.base.util.AssertUtil;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.implementation.bytecode.assign.Assigner;
 
@@ -7,31 +8,41 @@ import java.lang.reflect.Method;
 
 public class ByteBuddyAdvice {
 
-    @Advice.OnMethodEnter
-    public static Object onEnter(@Advice.This Object obj, @Advice.Origin Method method, @Advice.AllArguments Object[] args) {
+    private static Method enterMethod = null;
+    private static Method exitMethod = null;
+
+    static {
         try {
             Class<?> clazz = Class.forName("cn.gmlee.tools.agent.bytebuddy.ByteBuddyRegistry");
             Method[] methods = clazz.getMethods();
-            for (Method clazzMethod : methods) {
-                if (clazzMethod.getName().equals("enter")) {
-                    return clazzMethod.invoke(null, obj, method, args);
+            for (Method method : methods) {
+                if (method.getName().equals("enter")) {
+                    enterMethod = method;
+                } else if (method.getName().equals("exit")) {
+                    exitMethod = method;
                 }
             }
-        } catch (Exception ignored) {}
+            AssertUtil.notNull(exitMethod, "Tools ByteBuddyRegistry enterMethod not found.");
+            AssertUtil.notNull(exitMethod, "Tools ByteBuddyRegistry exitMethod not found.");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Tools ByteBuddyRegistry load fail.", e);
+        }
+    }
+
+    @Advice.OnMethodEnter
+    public static Object onEnter(@Advice.This Object obj, @Advice.Origin Method method, @Advice.AllArguments Object[] args) {
+        try {
+            return enterMethod.invoke(null, obj, method, args);
+        } catch (Exception ignored) {
+        }
         return null;
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class)
     public static void onExit(@Advice.Enter Object watcher, @Advice.Return(typing = Assigner.Typing.DYNAMIC) Object ret, @Advice.Thrown Throwable throwable) {
         try {
-            Class<?> clazz = Class.forName("cn.gmlee.tools.agent.bytebuddy.ByteBuddyRegistry");
-            Method[] methods = clazz.getMethods();
-            for (Method clazzMethod : methods) {
-                if (clazzMethod.getName().equals("exit")) {
-                    clazzMethod.invoke(null, watcher, ret, throwable);
-                    break;
-                }
-            }
-        } catch (Exception ignored) {}
+            exitMethod.invoke(null, watcher, ret, throwable);
+        } catch (Exception ignored) {
+        }
     }
 }
