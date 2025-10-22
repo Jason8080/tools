94659ce699f26a3f0ccc1a3b4c762eae298442f8
liqing <xiaoku13141@163.com>
2025-10-22 11:17:00 +0800

    代码优化

--- tools-base/src/main/java/cn/gmlee/tools/base/util/JvmUtil.java
+++ tools-base/src/main/java/cn/gmlee/tools/base/util/JvmUtil.java
@@ -4,52 +4,61 @@ package cn.gmlee.tools.base.util;
  * Java Virtual Machine 工具 .
  *
  * @author Jas °
- * @date 2021 /8/12 (周四)
  */
 public class JvmUtil {
 
     /**
      * 获取当前线程的全部堆栈信息.
      *
+     * @param msg the msg
      * @return the all msg
      */
     public static String getAllMsg(String... msg) {
-        StringBuffer sb = new StringBuffer();
-        if(BoolUtil.notEmpty(msg)){
-            sb.append(": ");
-            sb.append(String.join(" ", msg));
+        StringBuilder sb = new StringBuilder();
+        if (BoolUtil.notEmpty(msg)) {
+            sb.append(String.format("---------- %s ----------", String.join(" ", msg)));
         }
-        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
-        for (int i = 0; i < elements.length; i++) {
-            sb.append("\r\n");
-            sb.append("\t");
-            sb.append("at ");
-            sb.append(elements[i].getClassName());
-            sb.append(".");
-            sb.append(elements[i].getMethodName());
-            sb.append("(");
-            sb.append(elements[i].getClassName());
-            sb.append(":");
-            sb.append(elements[i].getLineNumber());
-            sb.append(")");
-        }
-        sb.insert(0, getStarter(elements[elements.length - 1]));
+        sb.append(getCurrentThreadStackTrace());
         return sb.toString();
     }
 
-    private static String getStarter(StackTraceElement element) {
-        StringBuffer sb = new StringBuffer();
-        sb.append(element.getClassName());
-        sb.append(": ");
-        sb.append("[");
-        sb.append(element.getMethodName());
-        sb.append(":");
-        sb.append(element.getLineNumber());
-        sb.append("/");
-        sb.append(Thread.currentThread().getName());
-        sb.append(":");
-        sb.append(Thread.currentThread().getId());
-        sb.append("]");
+    /**
+     * 获取当前线程的完整堆栈日志原文.
+     *
+     * @return the current thread stack trace
+     */
+    public static String getCurrentThreadStackTrace() {
+        return getThreadStackTrace(Thread.currentThread());
+    }
+
+    /**
+     * 获取指定线程的完整堆栈日志原文.
+     *
+     * @param thread the thread
+     * @return the thread stack trace
+     */
+    public static String getThreadStackTrace(Thread thread) {
+
+        if(thread == null){
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder(512);
+
+        sb.append("\r\nThread: ")
+                .append(thread.getName())
+                .append(" [id=")
+                .append(thread.getId())
+                .append(", state=")
+                .append(thread.getState())
+                .append("]\n");
+
+        for (StackTraceElement element : thread.getStackTrace()) {
+            sb.append("\tat ")
+                    .append(element)
+                    .append('\n');
+        }
+
         return sb.toString();
     }
 }
