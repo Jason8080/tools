d20ee91255786da361839680a79011dfc1319d64
Jas <xiaoku13141@163.com>
2026-07-27 19:33:36 +0800

    x

--- tools-im/src/main/java/cn/gmlee/tools/im/conf/ImProperties.java
+++
@@ -1,7 +0,0 @@
-package cn.gmlee.tools.im.conf;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "im")
-public class ImProperties {
-}
