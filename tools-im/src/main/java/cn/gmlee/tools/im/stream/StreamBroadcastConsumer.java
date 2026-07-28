f909e70618ff860e3f381b44a660ad2ed700efc5
Jas <xiaoku13141@163.com>
2026-07-28 10:06:54 +0800

    ...

--- tools-im/src/main/java/cn/gmlee/tools/im/stream/StreamBroadcastConsumer.java
+++ tools-im/src/main/java/cn/gmlee/tools/im/stream/StreamBroadcastConsumer.java
@@ -11,17 +11,6 @@ import java.util.function.Consumer;
 
 /**
  * Spring Cloud Stream 消息消费者
- * <p>
- * 作为 Spring Cloud Stream 的函数式消费者 Bean，
- * 接收来自其他实例的广播消息并路由到本地订阅者。
- * </p>
- * <p>
- * Bean 名称必须为 {@code sseImBroadcastConsumer}，
- * 对应 Spring Cloud Stream 绑定名 {@code sseImBroadcastConsumer-in-0}。
- * </p>
- *
- * @author SseIm Framework
- * @since 1.0.0
  */
 @Slf4j
 @RequiredArgsConstructor
